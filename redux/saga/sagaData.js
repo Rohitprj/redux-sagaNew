8ad8cae7ef560696d7e9189886951badef772463
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-04 20:47:54 +0530

    added comment

--- redux/saga/sagaData.js
+++ redux/saga/sagaData.js
@@ -1,6 +1,7 @@
 import { put, takeEvery } from "redux-saga/effects";
 import { SET_USER_DATA, USERS_DATA_LIST } from "../constants/addToCart";
 
+// Worker function
 function* apiCall() {
   const url = "https://dummyjson.com/products";
   let data = yield fetch(url);
@@ -9,6 +10,7 @@ function* apiCall() {
   // console.warn("Dummy data", data);
 }
 
+// Watcher function
 function* sagaData() {
   yield takeEvery(USERS_DATA_LIST, apiCall);
 }
